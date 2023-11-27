8969b70cc5317e3c5c0646e4d5fda9433c8a5fec
Chris eLabs <chris@elabs.enterprises>
2023-11-27 15:46:29 +0100

    Initial commit

---
+++ app/layouts/none.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div ref="el" :style="{ 'font-family': 'Blinker' }" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Blinker">
+    <slot/>
+  </div>
+</template>
+
+<style>
+</style>
